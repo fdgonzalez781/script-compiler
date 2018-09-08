be5a2254f1b43f2469ba740892b68f406fc4a623
Fernando Gonzalez <fallofrapture59@gmail.com>
2018-09-08 15:05:52 -0400

    Added working parser, code generator, updated test file

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,14 @@
 module Main where
 
+import System.Directory
+
 import Lib
+import Compiler
 
 main :: IO ()
-main = someFunc
+main = do
+  file <- readFileCurrentDir "src/script.txt"
+  putStrLn $ compile file
+
+readFileCurrentDir :: String -> IO String
+readFileCurrentDir str = getCurrentDirectory >>= \dir -> readFile $ dir ++ "/" ++ str
--- src/Compiler.hs
+++ src/Compiler.hs
@@ -1,35 +1,73 @@
-module Compiler where
+module Compiler (compile) where
 
 import Control.Monad
 import Text.Regex.PCRE
 import Data.Array
 import qualified Data.Text as Text
 import System.Directory
+import Data.List
+import Data.Maybe
+import System.Eval.Haskell
 
-data TokenType = Lambda | Var | Ident | Dot | Definition | OParen | CParen deriving (Eq, Show)
+-- lamdba f. lambda x. f x <=> \f -> \x -> f x
+-- lambda f. lamdba x. f x is equivalent to a value of type (a -> b) -> a -> a
+
+-- Examples of valid expressions:
+-- x (variable)
+-- f x (function application)
+-- f (a b) (function application on expr with parens)
+-- x0 x1 x2 ... xn (n-ary function application via currying)
+
+-- Grammar definition [expr]:
+-- expr ::= ident | ident expr | (expr)
+
+-- Example of valid statements:
+-- lambda x. x (bind param 1 to x, return x)
+-- lambda f. lambda x. f x (bind param 1 to f, bind param 2 to x, return f applied to x)
+-- func := lambda x. x (define function "func" as [bind var x, return x])
+-- (lambda f. lambda x. f x) T (bind param 1 to f, bind param 2 to x, return f applied to x, f := T, a lambda term)
+
+-- lambda x. x should parse to \x -> x.
+-- lambda f. lambda x. f x should parse to \f -> \x -> f x.
+-- (lambda f. lambda x. f x) T should parse to (\f -> \x -> f x) T.
+-- func := lambda x. x should parse to
+-- OPTIONAL: func :: a -> a
+-- REQUIRED: func = \x -> x OR func x = x
+
+data TokenType = Lambda | Ident | Dot | Defn | OParen | CParen | Endl deriving (Eq, Show)
 type Token = (TokenType, String)
 
-data Program = Program Statement Program | EndProgram deriving (Eq, Show)
+data VarDecl = Var String deriving (Eq, Show)
+data Term = Call VarDecl
+          | Abstr VarDecl Term
+          | Apply Term Term
+          deriving (Eq, Show)
 
-data Statement = BindVar { name :: String }
-               | BindIdent { name :: String }
-               | Apply { function :: String, arg :: String }
+data Statement = LambdaTerm Term
+               | Definition String Term
                deriving (Eq, Show)
+
+type Rule = [Token] -> Bool
+
+-- BNF Rules:
+-- term ::= ident | lambda ident term | term term
+-- statement ::= term | ident ':=' term
+
+type Program = [Statement]
 
 tokenTypes = [
   (Lambda, "\\blambda\\b"),
   (Ident, "\\b[a-zA-Z]+\\b"),
   (Dot, "\\."),
-  (Definition, ":="),
+  (Defn, ":="),
   (OParen, "\\("),
-  (CParen, "\\)")
+  (CParen, "\\)"),
+  (Endl, "\\;")
              ]
 
-readFileCurrentDir :: String -> IO String
-readFileCurrentDir str = getCurrentDirectory >>= \dir -> readFile $ dir ++ "/" ++ str
-
 compile :: String -> String
-compile = genCode . runParser . runLexer
+compile = genCode .
+  runParser . runLexer
 
 with :: (Traversable t) => t a -> (a -> b) -> t b
 with = flip fmap
@@ -51,15 +89,50 @@ tokenizeFirst [] = Nothing
 tokenizeFirst line = head $ filter (/= Nothing) $ with tokenTypes $ \(kind, ptn) ->
   matchOnceText (makeRegex ("\\A" ++ ptn) :: Regex) (strip line) >>= \(_, match, rem) -> Just ((kind, fst $ match ! 0), rem)
 
+breakAfter :: (a -> Bool) -> [a] -> ([a], [a])
+breakAfter p xs =
+  let (st, end) = break p xs
+  in case end of
+    [] -> (st, [])
+    _ -> (st ++ ([head end]), tail end)
+
 runParser :: [Token] -> Program
-runParser ((Lambda, _):(Ident, name):(Dot, _):xs) = Program (BindVar name) (runParser xs)
-runParser ((Ident, f):(Ident, x):xs) = Program (Apply f x) (runParser xs)
-runParser _ = EndProgram
+runParser tokens =
+  let (st, rem) = breakAfter (\token -> fst token == Endl) tokens
+  in case rem of
+    [] -> [matchStmt st]
+    _ -> matchStmt st : runParser rem
+
+matchTerm :: [Token] -> Term
+matchTerm ((Lambda, _):(Ident, x):(Dot, _):xs) = Abstr (Var x) (matchTerm xs)
+matchTerm ((OParen, x):xs) =
+  let (st, rem) = breakAfter (\(token, _) -> token == CParen) xs
+  in case rem of
+    [] -> matchTerm (init xs)
+    ((OParen, _):ys) -> Apply (matchTerm (init st)) (matchTerm (init rem))
+    ((Ident, z):[]) -> Apply (matchTerm (init st)) (Call (Var z))
+matchTerm ((Ident, x):y:xs) = case y of
+  (Lambda, _) -> Apply (Call (Var x)) (matchTerm (y:xs))
+  (Ident, _) -> Apply (Call (Var x)) (matchTerm (y:xs))
+  (OParen, _) -> Apply (Call (Var x)) (matchTerm (y:(takeWhile (\(token, _) -> token /= CParen) xs)))
+  _ -> Call (Var x)
+matchTerm ((Ident, x):[]) = Call (Var x)
+
+matchStmt :: [Token] -> Statement
+matchStmt ((Ident, name):(Defn, _):xs) = Definition name (matchTerm xs)
+matchStmt xs = LambdaTerm $ matchTerm xs
 
 genCode :: Program -> String
-genCode program = undefined
+genCode [] = []
+genCode (x:xs) = genCodeStmt x ++ "\n" ++ genCode xs
 
-testLexer :: IO ()
-testLexer = do
-  file <- readFileCurrentDir "src/script.txt"
-  print $ map fst $ runLexer file
+genCodeStmt :: Statement -> String
+genCodeStmt (Definition name term) = name ++ " = " ++ genCodeTerm term
+genCodeStmt (LambdaTerm term) = genCodeTerm term
+
+genCodeTerm :: Term -> String
+genCodeTerm (Call (Var name)) = name
+genCodeTerm (Abstr (Var name) term) = "\\" ++ name ++ " -> " ++ genCodeTerm term
+genCodeTerm (Apply (Call (Var x)) term) = x ++ " " ++ genCodeTerm term
+genCodeTerm (Apply term (Call (Var x))) = "(" ++ genCodeTerm term ++ ") " ++ x
+genCodeTerm (Apply t1 t2) = "(" ++ genCodeTerm t1 ++ ") (" ++ genCodeTerm t2 ++ ")"
